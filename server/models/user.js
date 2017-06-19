c9ab3015bdf1bca769649ffe1ccd6a63a14026da
Ajinkya Kolhe <ajinkya.k15@gmail.com>
2017-06-19 22:07:39 +1000

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,58 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash')
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   email: {
     require: true,
     trim: true,
     type: String,
-    minlength: 1
-  }
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength:6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      require: true
+    },
+    token: {
+      type: String,
+      require: true
+    }
+  }]
 });
+
+UserSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+// function is used instead of Arrow function to associate this keyword.
+// Arrow function does not associate this keyword
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+  user.tokens.push({access, token});
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {
   User
