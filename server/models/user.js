227dbf14a7a7c5cf4216f419ad551645032e0f69
Ajinkya Kolhe <ajinkya.k15@gmail.com>
2017-06-28 22:40:13 +1000

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -73,6 +73,25 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      })
+    });
+  });
+};
+
 //Mongoose middleware
 UserSchema.pre('save', function (next) {
   var user = this;
